34ea484ba15b8cc49d6dffe8dd8a581502fb2cae
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-29 14:53:25 -0500

    Adding fields

--- Models/Survey.cs
+++ Models/Survey.cs
@@ -2,5 +2,13 @@
 {
     public class Survey
     {
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Language { get; set; }
+
+        public string Comment { get; set; }
     }
 }
